306acf1c5e9a054487cb47ab3b6abd57c96e25a9
akira <akiraeiphoto@gmail.com>
2018-08-09 16:20:10 +0900

    add date time picker at recordPC and CC

--- client/src/components/record/RecordCC.js
+++ client/src/components/record/RecordCC.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import RecordPC from "./RecordPC";
 
+var moment = require("moment");
+
 class RecordCC extends Component {
   state = {
     calories: 3,
     balance: 3,
     tasty: 3,
-    category: ""
+    category: "",
+    date: moment().format("YYYY/MM/DD"),
+    tiem: moment().format("HH:mm")
   };
 
   handleChange = e => cate => {
--- client/src/components/record/RecordPC.js
+++ client/src/components/record/RecordPC.js
@@ -1,26 +1,44 @@
 import React, { Component } from "react";
-import { Radio, Rate } from "antd";
+import { Radio, Rate, DatePicker, TimePicker } from "antd";
 
 var moment = require("moment");
 
-const RadioGroup = Radio.Group;
+const { MonthPicker, RangePicker } = DatePicker;
 
 class RecordPC extends Component {
   render() {
     return (
       <React.Fragment>
-        <RadioGroup
+        <div>
+          <DatePicker
+            defaultValue={moment()}
+            format={"YYYY/MM/DD"}
+            onChange={e => {
+              this.props.onChange(e)("date");
+            }}
+          />
+          <TimePicker
+            defaultValue={moment()}
+            format={"HH:mm"}
+            onChange={e => {
+              this.props.onChange(e)("time");
+            }}
+          />
+        </div>
+
+        <Radio.Group
           onChange={e => {
             this.props.onChange(e)("category");
           }}
+          buttonstyle={"solid"}
         >
-          <Radio value={"Breakfast"}>Breakfast</Radio>
-          <Radio value={"Brunch"}>Brunch</Radio>
-          <Radio value={"Lunch"}>Lunch</Radio>
-          <Radio value={"Snack"}>Snack</Radio>
-          <Radio value={"Dinner"}>Dinner</Radio>
-          <Radio value={"Midnight"}>Midnight</Radio>
-        </RadioGroup>
+          <Radio.Button value={"Breakfast"}>Breakfast</Radio.Button>
+          <Radio.Button value={"Brunch"}>Brunch</Radio.Button>
+          <Radio.Button value={"Lunch"}>Lunch</Radio.Button>
+          <Radio.Button value={"Snack"}>Snack</Radio.Button>
+          <Radio.Button value={"Dinner"}>Dinner</Radio.Button>
+          <Radio.Button value={"Midnight"}>Midnight</Radio.Button>
+        </Radio.Group>
 
         <div>
           {"Calories: "}
