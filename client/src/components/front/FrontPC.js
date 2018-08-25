5f9f987c3a9b8526aaf6f32adab7e837b9dc4526
akira <akiraeiphoto@gmail.com>
2018-08-25 18:02:28 +0900

    Styling front pc

--- client/src/components/front/FrontPC.js
+++ client/src/components/front/FrontPC.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Card, Row, Col } from "antd";
 
 class FrontPC extends Component {
   render() {
     return (
       <React.Fragment>
-        <Link to={localStorage.getItem("token") ? "/record" : "/login"}>
-          <div>Welcome</div>
-          <div>meal - I - Own</div>
-        </Link>
+        <div className={"front-container"}>
+          <Link to={localStorage.getItem("token") ? "/record" : "/login"}>
+            <Card className={"front-card"}>
+              <Row type="flex" justify="center" align="center">
+                <Col>
+                  <div className={"front-postline"}>Welcome</div>
+                </Col>
+              </Row>
+              <Row type="flex" justify="center" align="center">
+                <Col>
+                  <div className={"front-postline"}>meal - I - Own</div>
+                </Col>
+              </Row>
+            </Card>
+          </Link>
+        </div>
       </React.Fragment>
     );
   }
